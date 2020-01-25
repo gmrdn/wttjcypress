786bc55672799dd8e4c14e7a81071f2e13aefe78
Guillaume R <guillaume.rondon@gmail.com>
2020-01-25 21:28:49 +0100

    verification changement d'url et création de carte

--- cypress/integration/welcomekit/dashboard/creation_de_candidat/creation_de_candidat.js
+++ cypress/integration/welcomekit/dashboard/creation_de_candidat/creation_de_candidat.js
@@ -18,9 +18,15 @@ describe('Le formulaire de création de candidat', function () {
         cy.contains("Enregistrer")
             .should('be.enabled')
             .click();
-        
+
         cy.wait('@enregistrement')
             .should('have.property', "status", 201)
+
+        cy.url()
+            .should('include','/dashboard/o/dkxzma3/jobs/2XMOI_yq66e6b')
+        
+        cy.get('li.card-thumbnail-name').first()
+            .should('have.text','Fleur Delisse');
     })
 
     it('affiche un message d\'erreur quand le serveur refuse l\'enregistrement (erreur 500)', function () {
@@ -47,11 +53,37 @@ describe('Le formulaire de création de candidat', function () {
     it('vérifie la validité de l\'adresse email', function () {
         cy.get("input[name='firstname']").type("Fleur")
         cy.get("input[name='lastname']").type("Delisse")
-        cy.get("input[name='email']").type("adresse@mauvaisformat")
+
+        cy.get("input[name='email']").type("nom@mauvaisformat")
+        cy.contains("Enregistrer")
+            .should('be.disabled')
+
+        cy.get("input[name='email']").clear().type("nom@bonformat.com")
+        cy.contains("Enregistrer")
+            .should('be.enabled')
+
+        cy.get("input[name='email']").clear()
         cy.contains("Enregistrer")
             .should('be.disabled')
     })  
+    
+    it('vérifie l\'upload de fichiers', () => {
+        cy.fixture('text.txt', 'base64').then(fileContent => {
+            cy.get("input[name='image']")
+            .upload({ fileContent, fileName: 'text.txt', mimeType: 'text/plain' }, { subjectType: 'input' });
+        });
+        cy.get('span.error-message')
+            .should('be.visible')
+            .and('have.text', 'Fichiers autorisés: jpg, jpeg, png, gif')
+        
+        cy.fixture('text.txt').then(fileContent => {
+            cy.get("input[name='image']")
+            .upload({ fileContent, fileName: 'logo.png', mimeType: 'image/png' }, { subjectType: 'input' });
+        });
 
+        cy.get('span.error-message')
+            .should('not.exist')
+    });
 })
 
 
